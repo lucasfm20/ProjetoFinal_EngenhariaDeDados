2f1e68d05fe26e9b66ee1a9bdb13ed984230373a
Lucas <lucasfm801@gmail.com>
2024-11-18 20:24:48 -0300

    Adicao do script do banco e do backup do mesmo

--- MassaDeDados/main.py
+++ MassaDeDados/main.py
@@ -7,8 +7,9 @@ fake = Faker()
 
 def data_ultimo_ano():
     hoje = datetime.today()
-    tres_anos_atras = hoje - timedelta(days=3*365)
+    tres_anos_atras = hoje - timedelta(days=3 * 365)
     return fake.date_between(start_date=tres_anos_atras, end_date=hoje)
+
 
 dados_cliente = []
 dados_imovel = []
@@ -17,8 +18,10 @@ dados_corretor = []
 dados_cobertura = []
 dados_apolice_seguro = []
 
+
+nomes_seguradoras = set()
+
 for i in range(10000):  
-    
     cliente = {
         'cliente_id': i + 1,
         'nome_cliente': fake.name(),
@@ -37,9 +40,16 @@ for i in range(10000):
     }
     dados_imovel.append(imovel)
 
+
+    while True:
+        nome_seguradora = fake.company()
+        if nome_seguradora not in nomes_seguradoras:
+            nomes_seguradoras.add(nome_seguradora)
+            break
+
     seguradora = {
         'seguradora_id': i + 1,
-        'nome_seguradora': fake.company(),
+        'nome_seguradora': nome_seguradora,
         'endereco_seguradora': fake.address(),
         'telefone_seguradora': fake.phone_number(),
         'email_seguradora': fake.email(),
@@ -77,11 +87,13 @@ for i in range(10000):
     }
     dados_apolice_seguro.append(apolice)
 
+
 def salvar_csv(nome_arquivo, dados, fieldnames):
     with open(nome_arquivo, mode='w', newline='', encoding='utf-8') as file:
         writer = csv.DictWriter(file, fieldnames=fieldnames)
         writer.writeheader()
         writer.writerows(dados)
+
 
 salvar_csv('clientes.csv', dados_cliente, ['cliente_id', 'nome_cliente', 'endereco_cliente', 'telefone_cliente', 'email_cliente'])
 salvar_csv('imoveis.csv', dados_imovel, ['imovel_id', 'cliente_id', 'endereco_imovel', 'tipo_imovel', 'valor_imovel'])
@@ -91,4 +103,3 @@ salvar_csv('coberturas.csv', dados_cobertura, ['cobertura_id', 'descricao', 'pre
 salvar_csv('apolices_seguro.csv', dados_apolice_seguro, ['apolice_id', 'cliente_id', 'imovel_id', 'seguradora_id', 'corretor_id', 'data_inicio', 'data_termino'])
 
 print('Arquivos CSV salvos com sucesso!')
-
